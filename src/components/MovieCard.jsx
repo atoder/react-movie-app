f008a1334c7bbf7efb9f8ca175ac1102ebc6de3d
AT <alexandert@gmail.com>
2025-10-15 13:54:08 -0700

    adding image to the movie posters

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie:
+  {title, vote_average, poster_path, release_date, original_language}
+}) => {
   return (
-    <div>
-      <p key={movie.id} className="text-white">{movie.title}</p>
+    <div className="movie-card">
+      <img
+        src={poster_path ?
+          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+      />
     </div>
   )
 }
